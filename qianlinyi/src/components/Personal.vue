12fae18bc7e46ea0738697ab2df61d0c0ca1ab62
zaizai <327120052@qq.com>
2022-10-10 22:27:58 +0800

    20221010 添加了个人信息

---
+++ qianlinyi/src/components/Personal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>钱霖奕</div>
+    <div>电话：(+86)15189172699</div>
+    <div>电子邮件：qianlinyi@hhu.edu.cn</div>
+    <div>南京市江宁区秣陵街道佛城西路8号河海大学</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+}
+</script>
+
+<style scoped>
+
+</style>
